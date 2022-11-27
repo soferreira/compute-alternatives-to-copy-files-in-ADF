e9f61836e832d2c90af646f8a8b67b98cd70e4f7
Yoav Dobrin <yodobrin@microsoft.com>
2022-11-27 11:55:18 +0200

    aca first drop

---
+++ aca/Controllers/BlobController.cs
@@ -0,0 +1,133 @@
+using Microsoft.AspNetCore.Mvc;
+using Azure.Storage.Blobs;
+using Azure.Storage.Sas;
+using Azure.Storage.Blobs.Models;
+using Azure;
+
+namespace aca;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BlobController : ControllerBase
+{
+    private readonly ILogger<BlobController> _logger;
+    private IConfiguration _configuration;
+
+    const string BLOB = "b";
+    const string CONTAINER = "c";
+
+    public BlobController(ILogger<BlobController> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+    }
+
+
+    [HttpPost]
+    public async Task<ActionResult<string>> CopyBlob(BlobRequest item)
+    {        
+        // validate inputs
+        if(item.Validate())
+        {
+        _logger.LogInformation($"BlobController::CopyBlob  {item.BlobName} starts");
+        string sourceCS = _configuration.GetValue<string>(item.SourceCS);
+        string targetCS = _configuration.GetValue<string>(item.TargetCS);
+
+        BlobContainerClient sourceBlobClient = new BlobContainerClient(sourceCS,item.SourceContainer);
+        BlobContainerClient targetBlobClient = new BlobContainerClient(targetCS,item.TargetContainer);
+        string sas = GetServiceSasUriForContainer(sourceBlobClient);
+        // create uri for the download
+        if(BLOB.Equals(item.RequestType) && !string.IsNullOrEmpty(sas)){
+            BlobClient sourceBlob = sourceBlobClient.GetBlobClient(item.BlobName);
+            BlobClient destBlob = targetBlobClient.GetBlobClient(item.BlobName);
+            await CopySingle(sourceBlob,destBlob,sas);            
+            
+            return $"Copied single blob {item.BlobName} from source:{item.SourceContainer}";
+            // copy single file
+        }else if(CONTAINER.Equals(item.RequestType)){
+            // copy entire container
+            await CopyContainer(sourceBlobClient,targetBlobClient,5000,sas);
+            return $"Copied container {item.SourceContainer} to {item.TargetContainer}";
+        }else{
+            // wrong type passed
+            return "Invalid input provided. ";
+        }
+      
+        }else{
+            _logger.LogInformation($"BlobController::CopyBlob::Validation error. ");            
+            return "Invalid input provided. ";
+        }
+        
+
+    }
+
+    private async Task CopySingle(BlobClient sourceBlob,BlobClient destBlob, string sas)
+    {
+        Uri uri = new Uri ($"{sourceBlob.Uri.AbsoluteUri}?{sas}");
+        // _logger.LogInformation($"the uri is:{uri}");
+        await destBlob.StartCopyFromUriAsync(uri);
+        // _logger.LogInformation($"BlobController::Copied single blob {sourceBlob.Name}");
+    }
+
+    private async Task CopyContainer(BlobContainerClient sourceBlobClient, BlobContainerClient targetBlobClient,  int? segmentSize, string sas)
+                                               
+    {
+        try
+        {
+            _logger.LogInformation("BlobController::CopyMulti starts");
+            // Call the listing operation and return pages of the specified size.
+            var resultSegment = sourceBlobClient.GetBlobsAsync()
+                .AsPages(default, segmentSize);
+
+            // Enumerate the blobs returned for each page.
+            await foreach (Azure.Page<BlobItem> blobPage in resultSegment)
+            {                
+                foreach (BlobItem blobItem in blobPage.Values)
+                {                    
+                    if(blobItem.Properties.ContentLength>0)
+                        await CopySingle(sourceBlobClient.GetBlobClient(blobItem.Name),
+                                         targetBlobClient.GetBlobClient(blobItem.Name),sas);    
+                }
+                _logger.LogInformation("BlobController::CopyMulti copied page of blobs");
+                
+            }
+        }
+        catch (RequestFailedException e)
+        {
+            // might need to contain (not throw up) the exceptio, allowing failure copies to continue
+           _logger.LogInformation($"BlobController::CopyMulti failed with exception:{e}");
+           throw;
+        }
+    }
+
+    private string GetServiceSasUriForContainer(BlobContainerClient containerClient )
+    {
+        // Check whether this BlobContainerClient object has been authorized with Shared Key.
+        if (containerClient.CanGenerateSasUri)
+        {
+            // Create a SAS token that's valid for one hour.
+            BlobSasBuilder sasBuilder = new BlobSasBuilder()
+            {
+                BlobContainerName = containerClient.Name,
+                Resource = "b"
+            };
+
+            sasBuilder.ExpiresOn = DateTimeOffset.UtcNow.AddMinutes(10);
+            sasBuilder.SetPermissions(BlobSasPermissions.All);        
+            
+
+            Uri sasUri = containerClient.GenerateSasUri(sasBuilder);
+
+            return sasUri.AbsoluteUri.Split('?')[1];;
+        }
+        else
+        {
+            _logger.LogInformation(@"BlobContainerClient must be authorized with Shared Key 
+                            credentials to create a service SAS.");
+            return string.Empty;
+        }
+    }
+
+}
+
+
