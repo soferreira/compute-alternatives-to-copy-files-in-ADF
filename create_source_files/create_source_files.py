cec1490c2ea053865e29a6c15c556cddddfaf9af
soferreira <soferreira@microsoft.com>
2022-12-14 15:29:00 +0100

    test

--- create_source_files/create_source_files.py
+++ create_source_files/create_source_files.py
@@ -1,48 +1,34 @@
+from pathlib import Path
 import shutil
-import os
-import uuid
 import os, uuid
-from azure.identity import DefaultAzureCredential
+from zipfile import ZipFile
+import urllib.request
+#from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
+NUMBER_OF_FILES = 5
+# CONNECTION_STRING = req_body.get('connection_string')
+CONNECTION_STRING = 'DefaultEndpointsProtocol=https;AccountName=soferreirastorage;AccountKey=sflIyHtagKkRq5fRvoy5pKShdb5gyG0+yNraQkZ1OpBO8GHcG2eH2QS6YGagHK/1bVcE2Oz6twKe+AStty9uNg==;EndpointSuffix=core.windows.net'
 
-storage_account_name = None
-container_name = None
+# Download the file from `url` and save it locally:
+url = "https://raw.githubusercontent.com/soferreira/copy-alternatives/main/create_source_files/daily.data"
+urllib.request.urlretrieve(url, "daily.data")
 
-account_url = f"https://{storage_account_name}.blob.core.windows.net"
-default_credential = DefaultAzureCredential()
+# Create a BlobServiceClient object
+blob_service_client = BlobServiceClient.from_connection_string(CONNECTION_STRING)
+# Set the name of the container where you want to upload the files
+container_name = "samplesourcefiles"
+# Set the local directory where the files are located
+local_directory = Path.cwd()
+file_to_upload = os.path.join(local_directory, "daily.data")
 
-# Create the BlobServiceClient object
-blob_service_client = BlobServiceClient(account_url, credential=default_credential)
 
-# Create the container
-container_client = blob_service_client.create_container(container_name)
-
-# Create a file in the local data directory to upload and download
-local_file_name = str(uuid.uuid4()) + ".data"
-
-# Write text to the file
-file = open(file=local_file_name, mode='w')
-for _ in range(140): # will create files of 20kb  
-    file.write('{"dataModelName":"data_model_1","operation":"U","factory":1354010702,"lineId":14069,"date":"2022-06-23T00:00:00","feature1":2,"dim":55,"yield":38605}')
-file.close()
-
-# Create a blob client using the local file name as the name for the blob
-blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
-
-print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
-
-# Upload the created file
-with open(file=local_file_name, mode="rb") as data:
-    blob_client.upload_blob(data)
-
-TOTAL_OF_FILES = [1, 2, 5, 10]
-
-for i in TOTAL_OF_FILES:
-    if not os.path.exists(dir):
-        os.makedirs('{total_files}k-unzip')
-        os.makedirs('{total_files}k-zip')
-    for _ in range(i * 1000):
-        shutil.copyfile('daily.data', f'./{i}k-unzip/daily-{uuid.uuid4()}.data')
-    for _ in range(i):
-        shutil.copyfile('daily.zip', f'./{i}k-zip/daily-{uuid.uuid4()}.zip')
+# Create the files
+os.makedirs(f'{NUMBER_OF_FILES}k-unzip', exist_ok=True)
+for _ in range(NUMBER_OF_FILES):
+    # token = 'sp=racwdl&st=2022-12-14T13:59:29Z&se=2022-12-15T21:59:29Z&spr=https&sv=2021-06-08&sr=c&sig=t1EJFhXS6nRMyRF%2FCani%2FRoVUFDAYA2SukOQLw6Sk2g%3D'
+    file_name = f'./{NUMBER_OF_FILES}k-unzip/daily-{uuid.uuid4()}.data'
+    # shutil.copyfile('daily.data', file_name)
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=file_name)
+    with open(file=file_to_upload, mode="rb") as data:
+        blob_client.upload_blob(data)
